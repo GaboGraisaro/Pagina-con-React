f3268f9f2848751c0b5788b934bd3d7fd71b1509
GaboGraisaro <Graisaro97@hotmail.com>
2022-09-13 09:32:16 -0300

    second

--- calculadoraa/src/App.js
+++ calculadoraa/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import boton from './componentes/boton.js'
+import './componentes/boton.js'
 
 import FreeCodeCampLogo from './imagenes/FreeCodeCampLogo.png'
 function App() {
@@ -13,11 +13,25 @@ function App() {
         <div className='contenedor-calculadora'>
         <div className='fila'>
           <boton>1</boton>
+          <boton>2</boton>
+          <boton>3</boton>
           <boton>+</boton>
         </div>
         <div className='fila'></div>
+          <boton>4</boton>
+          <boton>5</boton>
+          <boton>6</boton>
+          <boton>-</boton>
         <div className='fila'></div>
+          <boton>7</boton>
+          <boton>8</boton>
+          <boton>9</boton>
+          <boton>*</boton>
         <div className='fila'></div>
+          <boton>=</boton>
+          <boton>0</boton>
+          <boton>*</boton>
+          <boton>/</boton>
         <div className='fila'></div>
         </div>
       </div>
--- calculadoraa/src/componentes/boton.js
+++ calculadoraa/src/componentes/boton.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import '../Stylesheet/boton.css';
+import '../Stylesheet/boton.css'
  
 function boton (props) {
   const esOperador = valor => {
+    // eslint-disable-next-line
     return isNaN(valor) && (valor != '.') && (valor != '=');
   }; 
     return (
      <div    //plantillas literales ``
-      className= {`boton-contenedor ${esOperador(props.children) ? 'operador' : null} ` .trimEnd()} > 
+      className={`boton-cont ${esOperador(props.children) ? 'operador' : null} ` } > 
         {props.children} 
      </div>
     );
